265415ddff493b151cef61bac73346b8a8251b40
Kevin Sheth <kns98@yahoo.com>
2022-09-10 12:21:27 +0800

    pseudo

--- DiffDir/Program.cs
+++ DiffDir/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//
+// Enumerate Dir 1 (superset)
+//    - put in bag - concurrent bag is ok 
+//    - hash code to fileinfo
+
+//
+// Enumerate dir 2 (subset)
+//    - put in bag - concurrent bag
+//    - hash code to FileInfo
+
+//
+// Set subtract 1 - 2
+// 
+
+//
+// Copy resulting files to specified dir
+//
