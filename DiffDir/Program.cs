0d43a8bc04da4a6bd2bce4de104faa36585b7d7e
Kevin Sheth <kns98@yahoo.com>
2022-09-11 12:35:45 +0800

    dirs

--- DiffDir/Program.cs
+++ DiffDir/Program.cs
@@ -1,10 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+string dir1 = @"D:\OneDrive";
+string dir2 = @"C:\Users\kevin\Pictures\iCloud Photos\Photos";
+
 //
 // Enumerate Dir 1 (superset)
 //    - put in bag - concurrent bag is ok 
 //    - hash code to fileinfo
+
+
 
 //
 // Enumerate dir 2 (subset)
