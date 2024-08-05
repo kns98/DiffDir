ae374caf1975855813cc984743954780c2dedb48
Kevin Sheth <kns98@yahoo.com>
2024-08-05 10:11:40 +0800

    Update Program.cs

--- DiffDir/Program.cs
+++ DiffDir/Program.cs
@@ -1,25 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 
-string dir1 = @"D:\OneDrive";
-string dir2 = @"C:\Users\kevin\Pictures\iCloud Photos\Photos";
+class Program
+{
+    static void Main(string[] args)
+    {
+        string dir1 = @"path\to\first\directory";
+        string dir2 = @"path\to\second\directory";
 
-//
-// Enumerate Dir 1 (superset)
-//    - put in bag - concurrent bag is ok 
-//    - hash code to fileinfo
+        var dir1Files = Directory.GetFiles(dir1);
+        var dir2Files = Directory.GetFiles(dir2);
 
+        var dir1Hashes = new ConcurrentDictionary<string, string>();
+        var dir2Hashes = new ConcurrentDictionary<string, string>();
 
+        // Compute hashes in parallel
+        Parallel.Invoke(
+            () => ComputeHashes(dir1Files, dir1Hashes),
+            () => ComputeHashes(dir2Files, dir2Hashes)
+        );
 
-//
-// Enumerate dir 2 (subset)
-//    - put in bag - concurrent bag
-//    - hash code to FileInfo
+        // Calculate sets
+        var s1 = dir1Hashes.Keys.Except(dir2Hashes.Keys).ToList();
+        var s2 = dir2Hashes.Keys.Except(dir1Hashes.Keys).ToList();
+        var s3 = dir1Hashes.Keys.Intersect(dir2Hashes.Keys).ToList();
 
-//
-// Set subtract 1 - 2
-// 
+        // Write results to files
+        File.WriteAllLines("S1.txt", s1);
+        File.WriteAllLines("S2.txt", s2);
 
-//
-// Copy resulting files to specified dir
-//
+        Console.WriteLine("Files unique to the first directory (S1):");
+        s1.ForEach(Console.WriteLine);
+
+        Console.WriteLine("\nFiles unique to the second directory (S2):");
+        s2.ForEach(Console.WriteLine);
+
+        Console.WriteLine("\nFiles common to both directories (S3):");
+        s3.ForEach(Console.WriteLine);
+    }
+
+    static void ComputeHashes(string[] files, ConcurrentDictionary<string, string> hashDictionary)
+    {
+        foreach (var file in files)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    var hashString = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    hashDictionary.TryAdd(hashString, file);
+                }
+            }
+        }
+    }
+}
